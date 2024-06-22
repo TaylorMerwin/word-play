a42d871d2324b0b987750887287ff3547756d65d
Taylor <taylor.merwin@gmail.com>
2024-06-21 22:07:19 -0700

    playing with shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
+'use client'
+
+import { Loader2 } from "lucide-react"
+import React, { useState } from 'react';
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const [count, setCount] = useState(0);
+
   return (
     <main>
-      <Button>Click me</Button>
+      <Button variant="ghost" onClick={() => setCount(count + 1)}>
+        <Loader2 className="mr-2 h-4 w-4 animate-spin" />Clicked {count} times</Button>
+      <p>You have clicked the button {count} times</p>
     </main>
   );
 }
