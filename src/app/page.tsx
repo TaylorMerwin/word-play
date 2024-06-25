3c81b67ac2a7ecf7483db6c2c7fade35c7578600
Taylor <taylor.merwin@gmail.com>
2024-06-24 20:35:59 -0700

    added tiptap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { Loader2 } from "lucide-react"
 import React, { useState } from 'react';
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import  Tiptap  from "@/components/ui/Tiptap";
 
 export default function Home() {
   const [count, setCount] = useState(0);
@@ -13,6 +14,7 @@ export default function Home() {
       <Button variant="ghost" onClick={() => setCount(count + 1)}>
         <Loader2 className="mr-2 h-4 w-4 animate-spin" />Clicked {count} times</Button>
       <p>You have clicked the button {count} times</p>
+      <Tiptap />
     </main>
   );
 }
